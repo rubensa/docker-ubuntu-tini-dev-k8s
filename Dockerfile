294f112982ad9860270efa6ac89666bb43f08e00
Ruben Suarez <rubensa@gmail.com>
2020-10-08 10:44:00 +0200

    Set image tag as 18.04

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM rubensa/ubuntu-tini-dev
+FROM rubensa/ubuntu-tini-dev:18.04
 LABEL author="Ruben Suarez <rubensa@gmail.com>"
 
 # Tell docker that all future commands should be run as root
--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 docker build --no-cache \
-  -t "rubensa/ubuntu-tini-dev-k8s" \
+  -t "rubensa/ubuntu-tini-dev-k8s:18.04" \
   --label "maintainer=Ruben Suarez <rubensa@gmail.com>" \
   .
